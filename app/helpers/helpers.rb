c22a6e78f24950034c41a1030ddeca6a250b4daa
Kayla Reopelle <kreopelle@gmail.com>
2018-07-17 01:30:42 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,9 +1,10 @@
 class Helpers
   def self.current_user(session)
     user = User.find_by(user_id: session[:id])
-    if !user ? false : true
+    
   end
 
-  def is_logged_in?
+  def self.is_logged_in?
   end
+  
 end
