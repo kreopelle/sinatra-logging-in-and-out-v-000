57bdbc8c153469052cdc956b9574336ba1b10b0f
Kayla Reopelle <kreopelle@gmail.com>
2018-07-17 01:28:33 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,11 +1,7 @@
 class Helpers
   def current_user(session)
     user = User.find_by(id: session[:id])
-    if user
-      false
-    else 
-      true
-    end
+    if user ? true : false
   end
 
   def is_logged_ind?
