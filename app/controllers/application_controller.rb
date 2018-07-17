93924269e7cdc4d5337dce9b41c277293d9428e1
Kayla Reopelle <kreopelle@gmail.com>
2018-07-17 01:21:00 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find()
+    @user = User.find(username: params[:username], password: params[:password])
 
   end
 
